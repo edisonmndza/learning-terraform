55ae8644e997d23154aa519bcd5b31e9198e6ce6
edison.mendoza <edison.mendoza@cloud.statcan.ca>
2019-09-26 18:56:20 +0000

    removed depends_on line and made everything indirect

--- Azure/Linux/centos/centos.tf
+++ Azure/Linux/centos/centos.tf
@@ -14,9 +14,8 @@ resource "azurerm_resource_group" "mendedi-rg" {
 resource "azurerm_virtual_network" "mendedi-linux-vnet" {
     name                = "${var.prefix}-vnet"
     location            = "${var.location}"
-    resource_group_name = "${var.rg}"
+    resource_group_name = "${azurerm_resource_group.mendedi-rg.name}"
     address_space       = ["${var.address_space}"]
-    depends_on          = ["azurerm_resource_group.mendedi-rg"]
 }
 
 resource "azurerm_subnet" "mendedi-linux-subnet" {
